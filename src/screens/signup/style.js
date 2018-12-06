8cae7192b50b31bba19895c3fdcb0ecc9bef6e77
saadhassaan <karim.khamwani@hotmail.com>
2018-12-07 00:54:03 +0500

    #5 signup screen completed

---
+++ src/screens/signup/style.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components/native';
+
+
+export const TextView = styled.View`
+    flex: 0.2 
+    paddingHorizontal: 25
+    paddingVertical: 15
+`;
+
+
+export const LoginButton = styled.View`
+flex: 0.4
+paddingHorizontal: 25
+paddingVertical: 10
+`;
+
+export const ForgetPassword = styled.Text`
+fontSize: 16
+fontWeight: 600
+color: white
+marginTop: 20
+`;
+
+export const RegisterButton = styled.View`
+flex: 0.2
+justifyContent: flex-start
+paddingHorizontal: 25
+paddingVertical: 10
+marginTop:10
+`;
+
+
+export const ContinueAsGuestArea = styled.View`
+flex: 0.25
+justifyContent: center
+alignSelf: center
+marginBottom:12
+backgroundColor: transparent
+`;
+
+export const ContinueText = styled.Text`
+fontSize: 16
+fontWeight: 900
+color: white
+`;
+
+export const AccountText = styled.Text`
+fontSize: 15
+fontWeight: 700
+marginBottom:5
+color:white
+`;
+
+export const NeedAnAccountArea = styled.View`
+flex: 0.5
+justifyContent: flex-end
+alignSelf: center
+backgroundColor: transparent
+`;
+
+export const ForgetPasswordArea = styled.View`
+flex: 0.2
+flexDirection: row
+justifyContent: flex-start
+alignSelf: center
+marginTop: 20
+backgroundColor:transparent
+`;
