0d51c35af5f5545a49575c415d55a4e9a28b43ca
Karim liaquat <karim.khamwani@hotmail.com>
2018-12-13 23:25:57 +0500

    screen flickering fixed , trasition config added

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,39 +1,49 @@
 import { createStackNavigator } from 'react-navigation';
 import Login from '../screens/login/login';
-import Signup from '../screens/signup/signup'
-import ForgetPassword from '../screens/ForgetPassword/Forgetpassword'
+import Signup from '../screens/signup/signup';
+import ForgetPassword from '../screens/ForgetPassword/Forgetpassword';
 import DrawerNavigator from './DrawerNavigator';
-
-
 
 
 export default AppNavigator = createStackNavigator({
   LoginScreen: {
     screen: Login,
-    navigationOptions : {
-      header : null
-    }
   },
   SignupScreen: {
     screen: Signup,
-    navigationOptions : {
-      header : null
-    }
   },
   ForgetPasswordScreen: {
     screen: ForgetPassword,
-    navigationOptions : {
-      header : null
-    }
   },
-  HomeScreen : {
-    screen : DrawerNavigator,
-    navigationOptions : {
-      header : null
-    }
-  }
+  HomeScreen: {
+    screen: DrawerNavigator,
+  },
 },
 {
-  initialRouteName : 'ForgetPasswordScreen'
-}
-);
+  initialRouteName: 'LoginScreen',
+  headerMode: 'none',
+  transitionConfig: () => ({
+    transitionSpec: {
+      duration: 500,
+    },
+    screenInterpolator: (sceneProps) => {
+      const { layout, position, scene } = sceneProps;
+      const { index } = scene;
+      const width = layout.initWidth;
+
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index],
+        outputRange: [0, 1],
+      });
+
+      const translateX = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [width, 0, 0],
+      });
+      return {
+        opacity,
+        transform: [{ translateX }],
+      };
+    },
+  }),
+});
