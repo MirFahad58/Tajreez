c13088d5432a70539ee85538dcc2f1ee47520457
saadhassaan <karim.khamwani@hotmail.com>
2018-12-16 23:44:22 +0500

    #10 Payments screen partial work

--- src/screens/Payments/Payments.js
+++ src/screens/Payments/Payments.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 import React, { Component } from 'react';
-import { View, FlatList,Text } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { HeaderComponent } from '../../common/Header/Header';
 
 
@@ -10,19 +10,44 @@ class Payments extends Component {
     return (
       <View style={{ flex: 1 }}>
         <HeaderComponent showBackButton onPress={() => navigation.goBack()} />
-        
-        <FlatList
-          data={[{id:1,name:"saad",email:"s.saad45@gmail.com"}]}
-          renderItem={({ data }) => (
-            <View style={{height:100, width:'100%',backgroundColor:'white'}}>
-            <Text >item.name</Text>
-            <Text>item.email</Text>
+          <View style={{ flex: 0.14, backgroundColor: 'white', paddingVertical: 20 }}>
+            <View style={{ flex: 1 }}>
+              <View style={{ flex: 0.3, flexDirection: 'row', marginHorizontal: 20 }}>
+
+                <View style={{ flex: 0.5, justifyContent: 'flex-start', flexDirection: 'row' }}>
+                  <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+                    {item.name}
+                  </Text>
+                </View>
+
+                <View style={{ flex: 0.2, justifyContent: 'flex-end', flexDirection: 'row' }}>
+                  <Text style={{ color: 'grey' }}>
+                    {item.status}
+                  </Text>
+                </View>
+
+                <View style={{ flex: 0.3, alignContent: 'flex-end', justifyContent: 'flex-end', flexDirection: 'row' }}>
+                  <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'green' }}>
+                    {item.price}
+                  </Text>
+                </View>
+
+              </View>
+              <View style={{ flex: 0.1, borderTopColor: 'lightgrey', borderTopWidth: 0.5, marginHorizontal: 15 }}></View>
+                  <View style={{ flex: 0.2, paddingLeft: 20 }}>
+                    <Text style={{ fontWeight: 'bold' }}>
+                      {item.location}
+                    </Text>
+                  </View>
+
+                  <View style={{ flex: 0.2, justifyContent: 'flex-end', paddingLeft: 20 }}>
+                    <Text style={{ color: 'grey' }}>
+                      {item.time}
+                    </Text>
+                  </View>
             </View>
-          )}
-          keyExtractor={item => item.id}
-        />
-
-
+          </View>
+        <View style={{ flex: 0.02 }}></View>
       </View>
     );
   }
